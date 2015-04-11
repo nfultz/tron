69d7c272292422259176abb6bbe429c7ede2735e
Neal Fultz <nfultz@gmail.com>
2015-04-11 12:31:26 -0700

    Adding equipTron for package developers.

--- R/equipTron.R
+++ R/equipTron.R
@@ -1,12 +1,17 @@
+#' Equip a package with tron
+#'
+#' Adds a tron startup hook to a packages \code{.onLoad} and \code{DESCRIPTION}.
+#' Run this from a package's source root directory.
+#'
 #' @export
 equipTron <- function() {
+  message("Adding tron hook to R/zzz.R")
   zzz <- system.file(package="tron", "zzz.R")
-
   out <- file.path(getwd(), "R", "zzz.R")
+  cat(readLines(zzz), sep="\n", file=out, append=TRUE)
 
-  lapply(readLines(zzz), cat, "\n", file=out, append=TRUE)
-
-  edit(file=out)
+  message("Adding tron to DESCRIPTION")
+  cat("Suggests: tron", sep="\n", file="DESCRIPTION", append=TRUE)
 
   invisible(TRUE)
 }
--- inst/zzz.R
+++ inst/zzz.R
@@ -2,7 +2,7 @@
 # This will activate tron on a whole package
 .onLoad <- function(libName,pkgName) {
    if(getOption("tron", FALSE) && requireNamespace("tron", quietly=TRUE)) {
-     message("Using tron on ", pkgName)
+     packageStartupMessage("Using tron on ", pkgName)
      tron:::tron.environment(parent.env(environment()))
    }
 }
