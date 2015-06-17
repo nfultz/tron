63d7cfc8e65bbe641ff681e2c0303818583e01ac
Neal Fultz <nfultz@gmail.com>
2015-06-16 17:23:24 -0700

    Removing old way of tracing packages

--- inst/zzz.R
+++
@@ -1,8 +0,0 @@
-
-# This will activate tron on a whole package
-.onLoad <- function(libName,pkgName) {
-   if(getOption("tron", FALSE) && requireNamespace("tron", quietly=TRUE)) {
-     packageStartupMessage("Using tron on ", pkgName)
-     tron::tron(parent.env(environment()))
-   }
-}
